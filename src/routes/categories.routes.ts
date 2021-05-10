5b5334a4845cb6f72c20781b22aa85c2e2e13179
joao <joaovlima07@hotmail.com>
2021-05-10 16:55:35 -0300

    category routes and repository added

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,25 @@
-import {Router} from 'express';
+import { Router } from 'express';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoutes = Router()
 
-categoriesRoutes.post('/categories', (req, res) => {
-    const {name, description} = req.body;
+const categoriesRepository = new CategoriesRepository()
+
+categoriesRoutes.post('/', (req, res) => {
+    const { name, description } = req.body;
+
+    const existing = categoriesRepository.findByName(name)
+    if(existing) return res.status(400).send('category already exists')
+
+    categoriesRepository.create({name, description});
+    
+    return res.status(201).json()
+});
+
+categoriesRoutes.get('/', (req, res) => {
+    const all = categoriesRepository.list();
+
+    return res.status(200).json(all)
 })
 
 export { categoriesRoutes }
