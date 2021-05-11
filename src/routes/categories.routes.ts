68cee33f262d78bba307fc4ca8ee9cb2c4f2eb36
joao <joaovlima07@hotmail.com>
2021-05-11 15:27:47 -0300

    listCategoriesUseCase added

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,17 +1,14 @@
 import { Router } from 'express';
 import { CategoriesRepository } from '../modules/cars/repositories/CategoriesRepository';
-import { categoryController} from '../modules/cars/useCases/createCategory/index';
+import { categoryController } from '../modules/cars/useCases/createCategory/index';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories/index'
 
 const categoriesRoutes = Router()
 
 const categoriesRepository = new CategoriesRepository()
 
-categoriesRoutes.post('/', (req,res)=>{ return categoryController.handle(req,res)});
+categoriesRoutes.post('/', (req, res) => { return categoryController.handle(req, res) });
 
-categoriesRoutes.get('/', (req, res) => {
-    const all = categoriesRepository.list();
-
-    return res.status(200).json(all)
-});
+categoriesRoutes.get('/', (req, res) => { return listCategoriesController.handle(req, res)});
 
 export { categoriesRoutes }
