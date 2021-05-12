945db9b27157daf8dff751c1493e8e6ae6484846
joao <joaovlima07@hotmail.com>
2021-05-11 21:02:52 -0300

    deleting useless imports

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,4 @@
 import express from 'express';
-import {categoriesRoutes} from './routes/categories.routes'
-import { specificationsRoutes } from './routes/specifications.routes';
 import { router } from './routes/index';
 
 const app = express();
