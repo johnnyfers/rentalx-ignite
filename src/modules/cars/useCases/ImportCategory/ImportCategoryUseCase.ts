f1002282529519269796bd698cbf14365552bb6b
joao <joaovlima07@hotmail.com>
2021-05-13 20:14:03 -0300

    creating usecase to import categories

---
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,18 @@
+import fs from 'fs';
+import csvParser from 'csv-parser';
+
+class ImportCategoryUseCase{
+    execute(file: Express.Multer.File):void{
+        const stream = fs.createReadStream(file.path);
+  
+        const parseFile = csvParser()
+
+        stream.pipe(parseFile)
+
+        parseFile.on('data', async(line)=>{
+            console.log(line)
+        })
+    }
+}
+
+export { ImportCategoryUseCase }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express';
 import { categoryController } from '../modules/cars/useCases/createCategory/index';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories/index'
+import multer from 'multer';
+import { importCategoryController } from '../modules/cars/useCases/ImportCategory';
+
+const upload = multer({
+    dest: './tmp'
+})
 
 const categoriesRoutes = Router()
 
@@ -8,5 +14,9 @@ const categoriesRoutes = Router()
 categoriesRoutes.post('/', (req, res) => { return categoryController.handle(req, res) });
 
 categoriesRoutes.get('/', (req, res) => { return listCategoriesController.handle(req, res)});
+
+categoriesRoutes.post('/import', upload.single('file'), (req, res) => { 
+    return importCategoryController.handle(req,res);
+})
 
 export { categoriesRoutes }
