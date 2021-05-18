1dceaac20c3117ca990ab41e403e9abfcf607881
joao <joaovlima07@hotmail.com>
2021-05-18 20:41:59 -0300

    refactoring Category model

--- src/modules/cars/repositories/implementations/SpecificationRepository.ts
+++ src/modules/cars/repositories/implementations/SpecificationRepository.ts
@@ -1,4 +1,4 @@
-import { Specification } from '../../models/Specification';
+import { Specification } from '../../entities/Specification';
 import { ICreateSpecificationDTO, ICreateSpecificationRepository } from '../ISpecificationRepository';
 
 class SpecificationRepository implements ICreateSpecificationRepository{
