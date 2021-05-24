6c3c839062ef1ac17b5d8bc737645c3937131570
joao <joaovlima07@hotmail.com>
2021-05-24 19:37:17 -0300

    avatar error need to be fixed

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,19 @@
 import { Router } from 'express';
+import { ensureAuthenticated } from '../middlewares/ensureAuthenticated'
 import { CreateUserController } from '../modules/accounts/useCases/createUser/CreateUserController';
 import { UpdateUserAvatarController } from '../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController';
+import multer from 'multer'
+import uploadConfig from '../config/Upload'
 
 const usersRoutes = Router();
+
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'))
 
 const createUserController = new CreateUserController()
 const updateUserController = new UpdateUserAvatarController()
 
 usersRoutes.post('/', createUserController.handle)
 
-usersRoutes.patch('/avatar', updateUserController.handle )
+usersRoutes.patch('/avatar', ensureAuthenticated , uploadAvatar.single('avatar'), updateUserController.handle )
 
-export { usersRoutes}
+export { usersRoutes }
