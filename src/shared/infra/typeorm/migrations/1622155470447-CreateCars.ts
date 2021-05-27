a082353546ceaacfaebd123135ed7b60230153b4
joao <joaovlima07@hotmail.com>
2021-05-27 20:22:42 -0300

    creating CreateCars migration and updating tsconfig

---
+++ src/shared/infra/typeorm/migrations/1622155470447-CreateCars.ts
@@ -0,0 +1,68 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateCars1622155470447 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: 'cars',
+                columns: [
+                    {
+                        name: 'id',
+                        type: 'uuid',
+                        isPrimary: true
+                    },
+                    {
+                        name: 'name',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'description',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'daily_rate',
+                        type: 'numeric'
+                    },
+                    {
+                        name: 'available',
+                        type: 'boolean',
+                        default: true 
+                    },
+                    {
+                        name: 'license_plate',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'brand',
+                        type: 'varchar'
+                    },
+                    {
+                        name: 'category_id',
+                        type: 'uuid',
+                        isNullable: true
+                    },
+                    {
+                        name: 'created_at',
+                        type: 'timestamp',
+                        default: 'now()'
+                    }
+                ],
+                foreignKeys: [
+                    {
+                        name: 'FKCategoryCar',
+                        referencedTableName: 'categories',
+                        referencedColumnNames: ['id'],
+                        columnNames: ['category_id'],
+                        onDelete: 'SET NULL',
+                        onUpdate: 'SET NULL'
+                    }
+                ]
+            })
+        )
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable('cars')
+    }
+}
